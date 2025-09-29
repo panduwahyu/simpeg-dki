be0b6af61288469482f16a63972413b8084ed305
zansky07 <mhdzans02@gmail.com>
2025-09-29 14:48:16 +0700

    fix bug tanggal upload pegawai

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,10 +11,14 @@ class PegawaiController extends Controller
     {
         $userId = Auth::id(); // Ambil ID user yang login
 
-        // Ambil semua data upload pegawai langsung dari mandatory_uploads
         $uploads = DB::table('mandatory_uploads')
             ->join('jenis_dokumen', 'mandatory_uploads.jenis_dokumen_id', '=', 'jenis_dokumen.id')
             ->join('periode', 'mandatory_uploads.periode_id', '=', 'periode.id')
+            ->leftJoin('dokumen', function ($join) {
+                $join->on('dokumen.jenis_dokumen_id', '=', 'mandatory_uploads.jenis_dokumen_id')
+                    ->on('dokumen.periode_id', '=', 'mandatory_uploads.periode_id')
+                    ->on('dokumen.user_id', '=', 'mandatory_uploads.user_id');
+            })
             ->where('mandatory_uploads.user_id', $userId)
             ->select(
                 'jenis_dokumen.id as dokumen_id',
@@ -22,11 +26,12 @@ class PegawaiController extends Controller
                 'periode.id as periode_id',
                 'periode.periode_key',
                 'mandatory_uploads.is_uploaded',
-                'mandatory_uploads.updated_at'
+                'dokumen.tanggal_unggah as tanggal_upload'
             )
             ->orderBy('periode.periode_key')
             ->orderBy('jenis_dokumen.nama_dokumen')
             ->get();
+
 
         // Hitung file yang belum diupload
         $belumUpload = $uploads->where('is_uploaded', 0);
