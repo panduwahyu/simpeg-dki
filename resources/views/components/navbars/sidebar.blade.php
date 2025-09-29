eb3cf8aa4a94f19739d952749c5c5a6aac77c10b
zansky07 <mhdzans02@gmail.com>
2025-09-29 14:47:44 +0700

    mengembalikan dashboard

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -44,8 +44,8 @@
                 <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Pages</h6>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'monitoring' ? 'active bg-gradient-primary' : '' }}"
-                    href="{{ route('monitoring.index') }}">
+                <a class="nav-link text-white {{ $activePage == 'dashboard' ? 'active bg-gradient-primary' : '' }}"
+                    href="{{ route('dashboard') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">dashboard</i>
                     </div>
@@ -93,6 +93,16 @@
                 </a>
             </li>
             
+            <li class="nav-item">
+                <a class="nav-link text-white {{ $activePage == 'monitoring' ? 'active bg-gradient-primary' : '' }}"
+                    href="{{ route('monitoring.index') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">format_textdirection_r_to_l</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Monitoring</span>
+                </a>
+            </li>
+
             <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'rtl' ? 'active bg-gradient-primary' : '' }}"
                     href="{{ route('rtl') }}">
