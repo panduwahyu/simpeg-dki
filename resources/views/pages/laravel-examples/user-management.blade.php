6728e5d14356f1c45d09604607fe418b3202a333
Muliani Silalahi <222212777@stis.ac.id>
2025-10-04 15:04:39 +0700

    ngerapihin lg

--- resources/views/pages/laravel-examples/user-management.blade.php
+++ resources/views/pages/laravel-examples/user-management.blade.php
@@ -2,9 +2,7 @@
 
     <x-navbars.sidebar activePage="user-management"></x-navbars.sidebar>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-        <!-- Navbar -->
         <x-navbars.navs.auth titlePage="Manajemen Pegawai"></x-navbars.navs.auth>
-        <!-- End Navbar -->
 
         <div class="container-fluid py-4">
             <div class="row">
