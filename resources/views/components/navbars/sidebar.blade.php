f87dec579d049ba9f42361d72e45d9ecfa22f7e8
Muliani Silalahi <222212777@stis.ac.id>
2025-10-08 21:31:17 +0700

    ngubah tampilan aja si

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -6,9 +6,9 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href="{{ route('dashboard') }}">
-            <img src="{{ asset('assets/img/logo-ct.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-2 font-weight-bold text-white">SIPETRA</span>
+        <a class="navbar-brand d-flex align-items-center" href="{{ route('dashboard') }}">
+            <img src="{{ asset('assets/img/logo-ct.png') }}" class="navbar-brand-img" style="height: 55px;" alt="main_logo">
+            <span class="ms-2 fw-bold text-white" style="font-size: 2rem; line-height: 1;">SIPETRA</span>
         </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,15 +1,25 @@
 <x-layout bodyClass="bg-gray-200">
     <main class="main-content mt-0">
         <div class="page-header align-items-start min-vh-100"
-            style="background-image: url('/assets/img/bps.jpg'); background-position: center; background-size: cover;">
+             style="background-image: url('/assets/img/bps.jpg'); background-position: center; background-size: cover;">
+            
+            {{-- Overlay gelap --}}
             <span class="mask bg-gradient-dark opacity-6"></span>
 
             <div class="container mt-5">
-                <div class="row signin-margin">
-                    <div class="col-lg-4 col-md-8 col-12 mx-auto">
-                        <div class="card z-index-0 fadeIn3 fadeInBottom">
-                            
+                <div class="row min-vh-100 px-8">
 
+                    {{-- Kiri: Branding --}}
+                    <div class="col-lg-6 d-none d-lg-flex flex-column justify-content-center align-items-center text-center"
+                        style="color: white;">
+                        <img src="{{ asset('assets/img/logo-ct.png') }}" alt="Logo" class="mb-3" style="width: 160px;">
+                        <h1 class="fw-bold mb-2 text-white" style="font-size: 2.5rem;">SIPETRA</h1>
+                        <p class="fw-semibold mb-0" style="font-size: 1.1rem;">Sistem Informasi Pegawai Terpadu</p>
+                    </div>
+
+                    {{-- Kanan: Login Card --}}
+                    <div class="col-lg-6 col-md-8 col-12 d-flex align-items-center justify-content-center">
+                        <div class="card z-index-0 fadeIn3 fadeInBottom w-100" style="max-width: 400px; padding: 1.5rem; box-shadow: 0 10px 25px rgba(0,0,0,0.2);">
                             <div class="card-body">
                                 <form role="form" method="POST" action="{{ route('login') }}" class="text-start">
                                     @csrf
@@ -23,6 +33,7 @@
                                         </div>
                                     @endif
 
+                                    {{-- Email --}}
                                     <div class="input-group input-group-outline mt-3">
                                         <label class="form-label">Email</label>
                                         <input type="email" class="form-control" name="email" value="admin@material.com">
@@ -31,6 +42,7 @@
                                         <p class='text-danger inputerror'>{{ $message }}</p>
                                     @enderror
 
+                                    {{-- Password --}}
                                     <div class="input-group input-group-outline mt-3">
                                         <label class="form-label">Password</label>
                                         <input type="password" class="form-control" name="password" value="secret">
@@ -39,17 +51,17 @@
                                         <p class='text-danger inputerror'>{{ $message }}</p>
                                     @enderror
 
+                                    {{-- Remember me --}}
                                     <div class="form-check form-switch d-flex align-items-center my-3">
                                         <input class="form-check-input" type="checkbox" id="rememberMe">
                                         <label class="form-check-label mb-0 ms-2" for="rememberMe">Ingat saya</label>
                                     </div>
 
+                                    {{-- Submit --}}
                                     <div class="text-center">
                                         <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">
                                             Masuk
                                         </button>
-
-                                        {{-- Tombol login Google penuh --}}
                                         <a href="{{ route('google.login') }}" class="btn btn-light w-100 my-2">
                                             <i class="fa fa-google text-danger text-lg me-2"></i>
                                             Masuk dengan Google
@@ -59,23 +71,24 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div> <!-- page-header -->
     </main>
 
     @push('js')
         <script src="{{ asset('assets') }}/js/jquery.min.js"></script>
         <script>
             $(function () {
-                var text_val = $(".input-group input").val();
-                if (text_val === "") {
-                    $(".input-group").removeClass('is-filled');
-                } else {
-                    $(".input-group").addClass('is-filled');
-                }
+                $(".input-group input").each(function(){
+                    if($(this).val() === "") {
+                        $(this).closest('.input-group').removeClass('is-filled');
+                    } else {
+                        $(this).closest('.input-group').addClass('is-filled');
+                    }
+                });
             });
         </script>
     @endpush
-
 </x-layout>
